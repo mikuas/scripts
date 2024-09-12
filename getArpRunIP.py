9158654be86497a83e5b8bdf7009da5791adc245
Miku <null>
2024-09-12 19:09:26 +0800

    .

--- getArpRunIP.py
+++ getArpRunIP.py
@@ -5,20 +5,22 @@ def getArgsList():
     # 依次传入多个 target IP，最后一个是网关 IP
     parser = argparse.ArgumentParser(description="传参")
     parser.add_argument('--list', type=str, nargs="+", help='runHostIP', required=True)
-    parser.add_argument('--interface', type=str, help='网络接口名', default='eth0')
+    parser.add_argument('--interface', type=str, help='interfaceName', default='eth0')
+    parser.add_argument('--name', type=str, help='shFileName', required=False)
     return parser.parse_args()
 
 # 获取命令行参数
 result = getArgsList()
 
-target_ips = result.list[:-1]
-gateway_ip = result.list[-1]
-network_card = result.interface or 'eth0'
+targetIps = result.list[:-1]
+gatewayIp = result.list[-1]
+networkCard = result.interface or 'eth0'
+shFileName = result.name or 'arp_run.sh'
 
 processes = []
-for target_ip in target_ips:
+for targetIP in targetIps:
     # 以异步方式运行 arpspoof
-    proc = subprocess.Popen(['bash', 'arp_run.sh', target_ip, gateway_ip, network_card])
+    proc = subprocess.Popen(['bash', shFileName, targetIP, gatewayIp, networkCard])
     processes.append(proc)
 
 for proc in processes:

e7e5c61d60eed2034c5bd8cdcfcb1fe3113275ae
Miku <None>
2024-09-18 16:43:34 +0800

    .

--- getArpRunIP.py
+++ getArpRunIP.py
@@ -9,7 +9,7 @@ def getArgsList():
     parser.add_argument('-l', type=str, nargs="+", help='runHostIP', required=False)
     # 网卡名称
     parser.add_argument('-i', type=str, help='interfaceName', required=False)
-    # 网段 写了-l不执行 格式 192.168.1.1-192.168.1.253
+    # 网段 写了-l不执行 格式 192.168.1.1-253
     parser.add_argument('-n', type=str, help='ip-ip', required=False)
     # n的网关 写了n网段必写
     parser.add_argument('-r', type=str, help='ip-ip-RouteIP', required=False)
@@ -25,8 +25,10 @@ try:
     if not result.n:
         processes = []
         for targetIP in result.l[:-1]:
+            # print(f'Run IP: {targetIP}')
+            # print(f'Gateway IP : {result.l[-1]}')
             # 以异步方式运行 arpspoof
-            proc = subprocess.Popen(['bash', result.f or 'arp_run.sh', targetIP, result[-1], result.i or 'eth0'])
+            proc = subprocess.Popen(['bash', result.f or 'arp_run.sh', targetIP, result.l[-1], result.i or 'eth0'])
             processes.append(proc)
 
         for proc in processes:
@@ -39,12 +41,13 @@ try:
             # 以异步方式运行 arpspoof
             if netResult[0][:-len(netResult[0].split('.')[-1])] + str(targetIP) in (result.s or []):
                 continue
-            # print(netResult[0][:-len(netResult[0].split('.')[-1])] + str(targetIP))
+            # print(f'Run IP: {netResult[0][:-len(netResult[0].split('.')[-1])] + str(targetIP)}')
+            # print(f'Gateway IP : {result.r}')
             proc = subprocess.Popen(['bash', result.f or 'arp_run.sh', netResult[0][:-len(netResult[0].split('.')[-1])] + str(targetIP), result.r, result.i or 'eth0'])
             processes.append(proc)
 
         for proc in processes:
             # 等待所有的 arpspoof 进程完成
             proc.wait()
-except Exception as e:
-    print(e)
+except Exception:
+    pass
